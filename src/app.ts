b0d5cb9ebc1d63c2ad268a4e446de6ca516ff89f
Robiul Islam <robiulislamrobi0874@gmail.com>
2024-11-18 11:42:18 +0600

    basic project setup done

---
+++ src/app.ts
@@ -0,0 +1,30 @@
+import { Application, Request, Response } from 'express';
+import cors from 'cors';
+
+import express from 'express';
+const app: Application = express();
+
+//parser
+app.use(express.json());
+app.use(cors());
+app.get('/', (req: Request, res: Response) => {
+  const a = 1;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+  res.send(a);
+});
+
+export default app;
---
+++ src/app/config/index.ts
@@ -0,0 +1,15 @@
+import dotenv from 'dotenv';
+import path from 'path';
+
+// Configure dotenv to load .env file
+dotenv.config({ path: path.join(process.cwd(), '.env') });
+
+// process.cwd() gets the current working directory (CWD) of the Node.js process. This typically points to the root directory of your project.
+// Combines the current working directory (process.cwd()) with the string '.env' to create the full path to the .env file.
+// dotenv.config is called with the { path } option to tell dotenv where to find the .env filedotenv.config is called with the { path } option to tell dotenv where to find the .env file
+
+export default {
+  port: process.env.PORT,
+
+  database_url: process.env.DATABASE_URL,
+};
