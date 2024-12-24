9753a1bd12e7b3f9687cc468c2d171352b7b65db
Robiul Islam <robiulislamrobi0874@gmail.com>
2024-12-24 11:53:38 +0600

    Handle department validation when creating & updating document

--- src/app/modules/academicDepartment/academicDepartment.model.ts
+++ src/app/modules/academicDepartment/academicDepartment.model.ts
@@ -1,18 +1,49 @@
-import { model, Schema } from "mongoose";
-import { TAcademicDepartment } from "./academicDepartment.interface";
+import { model, Schema } from 'mongoose';
+import { TAcademicDepartment } from './academicDepartment.interface';
 
-const AcademicDepartmentSchema = new Schema<TAcademicDepartment>({
-	name: {
-	  type: String,
-	  required: true,
-	  unique:true
-	},
-	academicFaculty:{
-		type:Schema.Types.ObjectId,
-		ref:"AcademicFaculty"
-	}
+const AcademicDepartmentSchema = new Schema<TAcademicDepartment>(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    academicFaculty: {
+      type: Schema.Types.ObjectId,
+      ref: 'AcademicFaculty',
+    },
   },
   {
-	  timestamps:true
-  });
-  export const AcademicDepartment = model<TAcademicDepartment>("AcademicDepartment", AcademicDepartmentSchema)
+    timestamps: true,
+  },
+);
+
+
+// যদি আগেই একটা department same নামে থাকে তাহলে এইটা আটকায় দিবে
+AcademicDepartmentSchema.pre('save', async function (next) {
+	const isDepartmentExits = await AcademicDepartment.findOne({
+		name:this.name
+	});
+	if(isDepartmentExits){
+		throw new Error('This Department is already exists')
+	}
+	next()
+});
+
+// যদি ডিলিট করা কোন  department update করতে চায় তাহলে এই বেটা আটকায় দিবে
+AcademicDepartmentSchema.pre('findOneAndUpdate', async function (next) {
+	const query = this.getQuery() ;
+	// console.log(query) //{ _id: '676a437b3edcb9d9efe295f5' } = id ta dekhabe . jeita amra update korte chassi
+
+	const isDepartmentExits = await AcademicDepartment.findOne({
+		query
+	});
+	if(!isDepartmentExits){
+		throw new Error('This Department is already exists')
+	}
+	next()
+});
+export const AcademicDepartment = model<TAcademicDepartment>(
+  'AcademicDepartment',
+  AcademicDepartmentSchema,
+);
