53b77a7cf46a9821ee7db6aede34bbb608dd73ba
Robiul Islam <robiulislamrobi0874@gmail.com>
2024-12-29 17:04:28 +0600

    Convert Zod Error

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -9,6 +9,7 @@ dotenv.config({ path: path.join(process.cwd(), '.env') });
 // dotenv.config is called with the { path } option to tell dotenv where to find the .env filedotenv.config is called with the { path } option to tell dotenv where to find the .env file
 
 export default {
+  NODE_ENV:process.env.NODE_ENV,
   port: process.env.PORT,
   database_url: process.env.DATABASE_URL,
   bcrypt_solt_rounds: process.env.BCYPT_SALT_ROUNDS,
--- src/app/middleware/globalErrorHandler.ts
+++ src/app/middleware/globalErrorHandler.ts
@@ -1,5 +1,7 @@
 import { ErrorRequestHandler, NextFunction, Request, Response } from 'express';
 import { ZodError } from 'zod';
+import { TErrorSourses } from '../interface/error';
+import config from '../config';
 
 const globalErrorHandler:ErrorRequestHandler = (
   err,
@@ -8,24 +10,41 @@ const globalErrorHandler:ErrorRequestHandler = (
   next,
 ) => {
   let statusCode = err.statusCode || 500;
+  
   let message = err?.message || 'something went wrong bro from globalmiddleware';
-  type TErrorSourses = {
-    path:string | number,
-    message:string
-  }[]
-  const errorSources:TErrorSourses = [{
+  
+  let errorSources:TErrorSourses = [{
     path:"",
     message:"something went wrong bro from globalmiddleware"
   }]
+  const handleZodError = (err:ZodError)=>{
+    const errorSources:TErrorSourses = err.issues.map(issue => {
+     return {
+      path: issue?.path[issue.path.length - 1],
+      message:issue.message
+     }
+    })
+    const statusCode = 400;
+
+    return {
+      statusCode,
+      message: 'Zod Validation Error',
+      errorSources,
+    };
+  }
   if(err instanceof ZodError){
-    statusCode=400;
-    message='ami zod error'
+    const simplifiedError = handleZodError(err)
+    message = simplifiedError?.message;
+    errorSources = simplifiedError?.errorSources;
+    statusCode = simplifiedError.statusCode
+    console.log(simplifiedError)
   }
   res.status(statusCode).json({
     success: false,
     message,
-     amiError: err,
-   errorSources
+    //  amiError: err,
+   errorSources,
+   stack: config.NODE_ENV === 'development'? err?.stack : ''
   });
 };
 export default globalErrorHandler;
