fa84a472477ac4b27c0502a8e8a76b76bf2276de
Robiul Islam <robiulislamrobi0874@gmail.com>
2025-01-26 13:22:18 +0600

    create course validation

---
+++ src/app/modules/Course/course.validation.ts
@@ -0,0 +1,18 @@
+import { z } from "zod";
+const PreRequisteCoursesValidationSchema = z.object({
+	course:z.string(),
+	isDeleted:z.boolean().optional()
+})
+
+const createCourseValidationSchema = z.object({
+	body:z.object({
+		title:z.string(),
+		prefix:z.string(),
+		code:z.number(),
+		credits:z.number(),
+		preRequisteCourses: z.array(PreRequisteCoursesValidationSchema)
+	})
+})
+export const CourseValidation = {
+	createCourseValidationSchema
+}
