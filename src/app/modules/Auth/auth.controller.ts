71239ddd1cb9588f7dd061a9ec9da3866011a050
Robiul Islam <robiulislamrobi0874@gmail.com>
2025-03-05 16:21:11 +0600

    services and controller created for auth

---
+++ src/app/modules/Auth/auth.controller.ts
@@ -0,0 +1,22 @@
+
+import status from 'http-status';
+
+import sendResponse from '../../utils/sendResponse';
+import catchAsync from '../../utils/catchAsync';
+import { AuthServices } from './auth.service';
+
+
+const loginUser = catchAsync(async (req, res) => {
+ const result = await AuthServices.loginUser(req.body)
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'User is logged in succesfully!',
+    data: {
+   result
+    },
+  });
+});
+export const AuthControllers = {
+	loginUser
+}
---
+++ src/app/modules/Auth/auth.routes.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+import validateRequest from '../../middleware/validationRequest';
+import { AuthValidation } from './auth.validation';
+import { AuthControllers } from './auth.controller';
+
+
+const router = express.Router();
+
+router.post(
+  '/login',
+  validateRequest(AuthValidation.loginValidationSchema),
+  AuthControllers.loginUser,
+);
