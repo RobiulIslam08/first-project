9d5cb4d2595b2d2a87d185a979f51145254ee225
Robiul Islam <robiulislamrobi0874@gmail.com>
2025-03-06 15:23:11 +0600

    create interface for statics apply

--- src/app/modules/user/user.interface.ts
+++ src/app/modules/user/user.interface.ts
@@ -1,8 +1,13 @@
-export  type TUser = {
-	id:string,
-	password:string,
-	needPasswordChange: boolean,
-	role: 'admin' | 'student' | 'faculty',
-	status:'in-progress' | 'blocked',
-	isDeleted:boolean
+import { Model } from 'mongoose';
+
+export interface TUser {
+  id: string;
+  password: string;
+  needPasswordChange: boolean;
+  role: 'admin' | 'student' | 'faculty';
+  status: 'in-progress' | 'blocked';
+  isDeleted: boolean;
+}
+export interface UserModel extends Model<TUser> {
+  isUserExitsByCustomId(id:string):Promise<TUser>
 }
--- src/app/modules/user/user.model.ts
+++ src/app/modules/user/user.model.ts
@@ -1,8 +1,8 @@
 import { model, Schema } from 'mongoose';
-import { TUser } from './user.interface';
+import { TUser, UserModel } from './user.interface';
 import config from '../../config';
 import bcrypt from 'bcrypt';
-const userSchema = new Schema<TUser>({
+const userSchema = new Schema<TUser, UserModel>({
   id: {
     type: String,
     required: true,
@@ -49,4 +49,4 @@ userSchema.post('save', function (doc, next) {
   doc.password = '';
   next();
 });
-export const User = model<TUser>('User', userSchema)
+export const User = model<TUser,UserModel>('User', userSchema)
